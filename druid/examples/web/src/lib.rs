e7f6790cbbe547b85456d9498c05484feecfc83a
Robert Wittams <robert@wittams.com>
2021-01-07 14:50:10 +0000

    Include rotation on web

--- druid/examples/web/src/lib.rs
+++ druid/examples/web/src/lib.rs
@@ -72,6 +72,7 @@ impl_example!(list);
 impl_example!(multiwin);
 impl_example!(open_save);
 impl_example!(panels.unwrap());
+impl_example!(rotated);
 impl_example!(scroll_colors);
 impl_example!(scroll);
 impl_example!(split_demo);
